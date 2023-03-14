2281f51e7eb2cdf89679745f4d6ed457e7d23c7f
Sam Macbeth <smacbeth@duckduckgo.com>
2023-03-14 12:19:24 +0100

    Ensure no dynamic content scripts are registered before testing static ones

--- Shared (Extension)/Resources/tests/test.contentScripts.js
+++ Shared (Extension)/Resources/tests/test.contentScripts.js
@@ -3,6 +3,12 @@ import { loadPageAndWaitForLoad } from "./utils.js";
 const { expect } = chai;
 
 describe("content_scripts manifest entry", () => {
+  beforeEach(async () => {
+    if (chrome.scripting?.unregisterContentScript) {
+      await chrome.scripting.unregisterContentScripts();
+    }
+  });
+
   it("adds a script that runs on matching pages and communicate with the extension background", async () => {
     const messageReceived = new Promise((resolve) => {
       function messageListener(m) {
@@ -12,9 +18,9 @@ describe("content_scripts manifest entry", () => {
       chrome.runtime.onMessage.addListener(messageListener);
     });
     const tab = await loadPageAndWaitForLoad("https://example.com/");
-    const response = await messageReceived
-    chrome.tabs.remove(tab.id)
-    expect(response).to.be.an('object')
-    expect(response.greeting).to.equal('hello')
+    const response = await messageReceived;
+    chrome.tabs.remove(tab.id);
+    expect(response).to.be.an("object");
+    expect(response.greeting).to.equal("hello");
   });
 });
